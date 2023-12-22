e8af5f3b809ab20fcd8996a43b8c7002454ad3ae
Mikol Faro <mikol.faro@gmail.com>
2023-12-22 12:05:03 +0100

    Add OLED display

---
+++ README.md
@@ -0,0 +1,16 @@
+# Custom keymap
+
+## Key features
+
+- I2C communication
+- RGB LED to highlight active layers
+- Single LCD display that acts as the bridge between the halves
+
+## Hardware
+
+- [SparkFun Qwiic Pro Micro](https://www.sparkfun.com/products/15795)
+- [SparkFun Qwiic OLED Display](https://www.sparkfun.com/products/17153)
+
+## TODO
+
+- Design PCB replacement of OLED display PCB using TRRS connectors
--- keymap.c
+++ keymap.c
@@ -106,3 +106,29 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     return state;
 }
 
+#ifdef OLED_ENABLE
+bool oled_task_user(void) {
+    // Host Keyboard Layer Status
+    oled_write_P(PSTR("Layer: "), false);
+
+    switch (get_highest_layer(layer_state)) {
+        case _QWERTY:
+            oled_write_P(PSTR("Default\n"), false);
+            break;
+        case _FN:
+            oled_write_P(PSTR("FN\n"), false);
+            break;
+        default:
+            // Or use the write_ln shortcut over adding '\n' to the end of your string
+            oled_write_ln_P(PSTR("Undefined"), false);
+    }
+
+    // Host Keyboard LED Status
+    led_t led_state = host_keyboard_led_state();
+    oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
+    oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
+    oled_write_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
+
+    return false;
+}
+#endif
